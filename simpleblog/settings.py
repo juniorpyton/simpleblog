dd7a1cd4f6531e9e18decd54fcc080a1f65ebe6c
juniorpyton <teknoekip16@gmail.com>
2021-05-08 14:52:36 +0300

    first commit

--- simpleblog/settings.py
+++ simpleblog/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
@@ -140,6 +141,13 @@ STATICFILES_DIRS = (
         os.path.join(BASE_DIR,'static'),
     )
 
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 LOGIN_REDIRECT_URL ='home'
